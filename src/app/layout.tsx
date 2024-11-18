10c60060443384afe87df280cd4ab8c56d16ad4e
parv141206 <148616575+parv141206@users.noreply.github.com>
2024-11-18 19:10:51 +0530

    Updated Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import { ModalProvider } from "@/hooks/useModal";
 import "./globals.css";
 import { ConfirmProvider } from "@/hooks/useConfirm";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -10,6 +11,46 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-black">
+      <head>
+        <title>MongUI</title>
+        <meta
+          name="description"
+          content="MongUI is a web application that allows users to visually draw Mongoose models and extract them as code with a click of a button."
+        />
+        <meta
+          name="keywords"
+          content="MongUI, Mongoose, MongoDB, schema design, web application, code generation, visual modeling"
+        />
+        <meta name="author" content="Parv Shah and Rudra Mehta" />
+        <meta
+          property="og:title"
+          content="MongUI - Mongoose Model Drawing Tool"
+        />
+        <meta
+          property="og:description"
+          content="Create and extract Mongoose models effortlessly with MongUI."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mongui.vercel.app/" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </head>
       <ConfirmProvider>
         <ModalProvider>
           <body className={``}>{children}</body>
